f29b7fe67c3e4d7379d63a81e1069c9c2cf4faef
manujbhardwaj19 <manujbhardwaj19@ccs.neu.edu>
2018-01-27 12:31:11 -0500

    Integrating Spring Boot with MySQL

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cs5200-spring2018-bhardwaj
+## Integrating Spring Boot with MySQL
+
+[Insert a static hello message](cs5200-spring2018-bhardwaj.us-east-2.elasticbeanstalk.com/api/hello/insert)
+
+[Insert a parameterized hello message](cs5200-spring2018-bhardwaj.us-east-2.elasticbeanstalk.com/api/hello/insert/Some parameterized message)
+
+[Retrieve all hello messages](http://cs5200-spring2018-bhardwaj.us-east-2.elasticbeanstalk.com/api/hello/select/all)
--- src/main/java/edu/northeastern/cs5200/controllers/hello/HelloObject.java
+++ src/main/java/edu/northeastern/cs5200/controllers/hello/HelloObject.java
@@ -1,15 +1,38 @@
 package edu.northeastern.cs5200.controllers.hello;
-public class HelloObject {
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name="hello")
+public class HelloObject {    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
     private String message;
+    
     public String getMessage() {
         return message;
     }
+    
     public void setMessage(String message) {
         this.message = message;
     }
+    
     public HelloObject(String message) {
         this.message = message;
     }
+    
     public HelloObject() {
     }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
 }
+
